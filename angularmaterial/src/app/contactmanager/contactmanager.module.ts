24eb241fad4afdacf0a1c4d2c551c86a7c9dac82
Alexandru Turcu <alexandru.turcu@SVROLP01666>
2020-12-03 16:01:39 +0200

    Added dialog themes

--- angularmaterial/src/app/contactmanager/contactmanager.module.ts
+++ angularmaterial/src/app/contactmanager/contactmanager.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { MaterialModule } from '../shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ContactManagerAppComponent } from './contactmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
@@ -13,6 +13,7 @@ import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { UserService } from './services/user.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NotesComponent } from './components/notes/notes.component';
+import { NewContactDialogComponent } from './components/new-contact-dialog/new-contact-dialog.component';
 
 const routes: Routes = [
   { path: '', component: ContactManagerAppComponent,
@@ -26,13 +27,14 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [ContactManagerAppComponent, ToolbarComponent, MainContentComponent, SideNavComponent, NotesComponent],
+  declarations: [ContactManagerAppComponent, ToolbarComponent, MainContentComponent, SideNavComponent, NotesComponent, NewContactDialogComponent],
   imports: [
     CommonModule,
     HttpClientModule,
     MaterialModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   providers: [
